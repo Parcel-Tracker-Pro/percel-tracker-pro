da10e80662bb4cfb5eb676c53c34bd0199e7402d
aaog2 <aungaungoogame2@gmail.com>
2025-01-27 19:52:42 +0630

    pj init

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar/Navbar";
-import About from "./pages/About";
-// import Service from "./pages/Service";
-import ServicePage from "./pages/Service";
-import Contact from "./pages/Contact";
-import PageTransition from "./components/PageTransition";
-import Footer from "./components/Footer";
-import { AnimatePresence } from "framer-motion";
-import ScrollToTop from "./ScrollToTop";
-// import About from "./pages/About";
-// import Service from "./pages/Service";
-// import PageNotFound from "./pages/PageNotFound";
+import React from "react";
 
 function App() {
-  return (
-    <AnimatePresence>
-      <Router>
-        <div>
-          <ScrollToTop />
-          <Navbar />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <PageTransition>
-                  <Home />
-                </PageTransition>
-              }
-            />
-            <Route
-              path="/about"
-              element={
-                <PageTransition>
-                  <About />
-                </PageTransition>
-              }
-            />
-            <Route
-              path="/service"
-              element={
-                <PageTransition>
-                  <ServicePage />
-                </PageTransition>
-              }
-            />
-            <Route
-              path="/contact"
-              element={
-                <PageTransition>
-                  <Contact />
-                </PageTransition>
-              }
-            />
-            {/* <Route path="*" element={<PageNotFound />} /> */}
-          </Routes>
-        </div>
-        <Footer classData="bg-footer" />
-      </Router>
-    </AnimatePresence>
-  );
+  return <div>App</div>;
 }
 
 export default App;
